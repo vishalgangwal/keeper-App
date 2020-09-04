3aa1519ce9f1432bcad77ac8cad763ffd3e5ec50
vishalgangwal <gangwalvishal99@gmail.com>
2020-09-04 13:34:02 +0530

    Updates

---
+++ precache-manifest.228b06e4f014e77498fd60710e213a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f4bc52e17a4e754bb16c46c2df0613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e61453a9505e41bf7a10",
+    "url": "/static/css/main.3229a7ea.chunk.css"
+  },
+  {
+    "revision": "18503707233812f93ec5",
+    "url": "/static/js/2.83ebf036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83ebf036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61453a9505e41bf7a10",
+    "url": "/static/js/main.7136019c.chunk.js"
+  },
+  {
+    "revision": "6c9d89967d5c60bff79a",
+    "url": "/static/js/runtime-main.4b45e496.js"
+  }
+]);
