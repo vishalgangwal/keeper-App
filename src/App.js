fbe4c0541ff12c0ccdcaa1a6cda0f1ca49c5ebda
vishalgangwal <gangwalvishal99@gmail.com>
2020-09-06 13:57:17 +0530

    recommit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import Header from './sticky-note/header';
-import Footer from './sticky-note/footer';
+//import Footer from './sticky-note/footer';
 import Note from './sticky-note/note';
 import Createarea from './sticky-note/createArea.js';
 
@@ -34,8 +34,8 @@ function delNote(idName){
        { newNote.map((note,index)=> {
           return <Note remove={delNote}   key={index} id={index} title={note.title} content={note.content} />
         })}    
-
-      <Footer />      
+{/* 
+      <Footer />       */}
     </div>
   );
 }
